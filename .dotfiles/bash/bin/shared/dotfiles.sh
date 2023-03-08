c7051cc077afe7a815b3fca4b474ca9f5787cc59
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-03-08 16:02:55 -0600

    dotfiles.sh: Make more robust

--- .dotfiles/bash/bin/shared/dotfiles.sh
+++ .dotfiles/bash/bin/shared/dotfiles.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-git --git-dir="${HOME}/git/dotfiles" --work-tree="${HOME}" "${@}"
+git_dir="${HOME}/git/dotfiles/.git"
+disabled=$(git --git-dir="${git_dir}" --work-tree="${HOME}" config --bool disabled.${1} 2>/dev/null)
+
+if ${disabled:-false}; then
+    print "The %s command is intentionally disabled by this function.\ " "${1}"
+    print "Cautiously run the disabled command by calling git with correct parameters instead:\n"
+    print "git --git-dir=%s --work-tree=%s ...\n" "${git_dir}" "${HOME}"
+    exit 1
+fi
+
+# Sets the git directory to the dotfiles repo and work tree to home
+git --git-dir="${git_dir}" --work-tree="${HOME}" -c status.showUntrackedFiles=no "$@"
