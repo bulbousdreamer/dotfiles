dd079117fd10c54c2466dacb2d9252900c74f0f0
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-07-17 20:05:22 -0500

    git_fetch.sh: Fetch all the repositories in ~/git

--- .dotfiles/bash/bin/shared/git_fetch.sh
+++ .dotfiles/bash/bin/shared/git_fetch.sh
@@ -3,12 +3,13 @@
 repos=()
 while IFS= read -d '' -r; do
 	repos+=("${REPLY}")
-done < <(find "${DOTFILES_HOME}/git" -mindepth 1 -maxdepth 1 -type d -print0)
+done < <(find "${HOME}/git" -mindepth 1 -maxdepth 1 -type d -print0)
 
 for repo in "${repos[@]}"; do
 	case "$(basename "${repo}")" in
 	*)
-		git -C "${repo}" fetch --all --recurse-submodules ${1:-}
+		echo "Repo is: ${repo}"
+		git -C "${repo}" fetch --all --recurse-submodules "${@}"
 	;;
 	esac
 done
