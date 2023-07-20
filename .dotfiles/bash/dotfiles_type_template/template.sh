1effe761a0c01ce6d88366c140009b1b73c97f40
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-07-20 00:09:04 -0500

    Use DOTFILES_TYPE variable in debug prints instead of hardcoding because it is annoying

--- .dotfiles/bash/dotfiles_type_template/template.sh
+++ .dotfiles/bash/dotfiles_type_template/template.sh
@@ -6,4 +6,3 @@ mkdir ../${new_dir} && cd ../${new_dir} || exit 1
 cd ../${new_dir} || exit 1
 cp -r ../dotfiles_type_template/shared . || exit 1
 cp -r ../dotfiles_type_template/user . || exit 1
-find . -type f -exec sed -i -e "s/dotfiles_type_template/${new_dir}/g" {} \;
