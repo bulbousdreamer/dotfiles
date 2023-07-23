89a1dc1d83b630833c4d4973543d5a9d6acd5850
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-07-23 05:17:56 +0100

    Modify cygwin_laptop dotfiles.sh to pass Windows paths to Git Bash so hopefully it works

---
+++ .dotfiles/bash/cygwin_laptop/shared/bin/dotfiles.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git_dir="$(cygpath --mixed --absolute ${DOTFILES_REPO})"
+dotfiles_dir="$(cygpath --mixed --absolute ${DOTFILES_HOME})"
+
+disabled=$(git --git-dir="${git_dir}" --work-tree="${dotfiles_dir}" config --bool disabled.${1} 2>/dev/null)
+
+if ${disabled:-false}; then
+    print "The %s command is intentionally disabled by this function.\ " "${1}"
+    print "Cautiously run the disabled command by calling git with correct parameters instead:\n"
+    print "git --git-dir=%s --work-tree=%s ...\n" "${git_dir}" "${dotfiles_dir}"
+    exit 1
+fi
+
+# Sets the git directory to the dotfiles repo and work tree to home
+git --git-dir="${git_dir}" --work-tree="${dotfiles_dir}" -c status.showUntrackedFiles=no "$@"
