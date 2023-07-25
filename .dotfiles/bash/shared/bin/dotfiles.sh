286b4e5615f3aa8b737b1c6e9c233a14e3c34aae
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-07-25 19:46:26 +0100

    dotfiles.sh: Call printf instead of print which is a Windows exexecutable to print to a printer

--- .dotfiles/bash/shared/bin/dotfiles.sh
+++ .dotfiles/bash/shared/bin/dotfiles.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-git_dir="${DOTFILES_REPO}"
+[[  -e"${DOTFILES_REPO}" ]] && git_dir="${DOTFILES_REPO}" || printf
+
+
 dotfiles_dir="${DOTFILES_HOME}"
 
 disabled=$(git --git-dir="${git_dir}" --work-tree="${dotfiles_dir}" config --bool disabled.${1} 2>/dev/null)
 
 if ${disabled:-false}; then
-    print "The %s command is intentionally disabled by this function.\ " "${1}"
-    print "Cautiously run the disabled command by calling git with correct parameters instead:\n"
-    print "git --git-dir=%s --work-tree=%s ...\n" "${git_dir}" "${dotfiles_dir}"
+    printf "The %s command is intentionally disabled by this function.\ " "${1}"
+    printf "Cautiously run the disabled command by calling git with correct parameters instead:\n"
+    printf "git --git-dir=%s --work-tree=%s ...\n" "${git_dir}" "${dotfiles_dir}"
     exit 1
 fi
 
