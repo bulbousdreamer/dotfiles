a4bb40eb968d6bdd24d3eb099f81214d1fad6ee5
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-07-26 05:16:43 +0100

    Add cygwin wrappers for git diff etc and Notepad++

---
+++ .dotfiles/bash/cygwin_laptop/shared/bin/git_ext_diff_report.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+reminder that if new/old is null then the file name should come from old/new
+
+old_file="${1}"
+new_file="${2}"
+report_folder="${3}"
+script_file="${DOTFILES_HOME}/.dotfiles/bash/${DOTFILES_TYPE}/shared/bin/bcdiff.txt"
+
+if [ "${old_file}" == "/dev/null" ]; then
+    old_file="${DOTFILES_HOME}/.dotfiles/bash/${DOTFILES_TYPE}/shared/storage/null_file"
+    report_file="${report_folder}/$(echo "${new_file}" | sed 's,/,___,g').html"
+fi
+
+if [ "${new_file}" == "/dev/null" ]; then
+    new_file="${DOTFILES_HOME}/.dotfiles/bash/${DOTFILES_TYPE}/shared/storage/null_file"
+    report_file="${report_folder}/$(echo "${old_file}" | sed 's,/,___,g').html"
+fi
+
+mkdir -p "${report_folder}"
+touch "${report_file}"
+"$(cygpath --unix --absolute "C:/Program Files/Beyond Compare 4/BCompare.exe")" \
+  @"(cygpath --mixed --absolute "${script_file}")" \
+  "(cygpath --mixed --absolute "${old_file}")" \
+  "(cygpath --mixed --absolute "${new_file}")" \
+  "(cygpath --mixed --absolute "${report_file}")"
