9f11fb497c2b8eb183b69cc5669f34980bcbf0af
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2023-08-02 14:56:56 +0100

    cygwin_na_na git_ext_diff.sh: Correct error by output to file instead of folder and some cleanup

--- .dotfiles/bash/cygwin_na_na/shared/bin/git_ext_diff_report.sh
+++ .dotfiles/bash/cygwin_na_na/shared/bin/git_ext_diff_report.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash -u
 
 # export DIFF_LOG_FOLDER="${HOME}/logs/somedescription/$(date "+%Y%m%d%H%M%S%N")_${PWD##*/}" && git difftool --tool=bc4report somecommitorwhatever
-printf "1=%s\n" "${1}"
-printf "2=%s\n" "${2}"
-printf "3=%s\n" "${3}"
 
 old_file="$(cygpath --mixed --absolute "${1/cygdrive/proc\/cygdrive}")"
 new_file="$(cygpath --mixed --absolute "${2/cygdrive/proc\/cygdrive}")"
@@ -11,14 +8,6 @@ file_label="${new_file//\:/__}"
 report_folder="$(cygpath --mixed --absolute "${3/cygdrive/proc\/cygdrive}")"
 script_file="$(cygpath --mixed --absolute "${DOTFILES_HOME/cygdrive/proc\/cygdrive}/.dotfiles/bash/${DOTFILES_TYPE}/shared/bin/bcdiff.txt")"
 report_file="${report_folder}/${new_file//\//___}.html"
-
-# $(sed -e 's,/,___/g;s,:,___/g' <<< $V)
-
-printf "old_file=%s\n" "${old_file}"
-printf "new_file=%s\n" "${new_file}"
-printf "report_folder=%s\n" "${report_folder}"
-printf "script_file=%s\n" "${script_file}"
-printf "file_label=%s\n" "${file_label}"
 
 if [ "${old_file}" == "/dev/null" ]; then
     old_file="${DOTFILES_HOME}/.dotfiles/bash/${DOTFILES_TYPE}/shared/storage/null_file"
@@ -34,8 +23,8 @@ printf "report_file=%s\n" "${report_file}"
 
 mkdir -p "${report_folder}"
 touch "${report_file}"
-echo "C:/Program Files/Beyond Compare 4/BCompare.exe" \
+"C:/Program Files/Beyond Compare 4/BCompare.exe" \
   @"$(cygpath --mixed --absolute "${script_file}")" \
   "$(cygpath --mixed --absolute "${old_file}")" \
   "$(cygpath --mixed --absolute "${new_file}")" \
-  "$(cygpath --mixed --absolute "${report_folder}")"
+  "$(cygpath --mixed --absolute "${report_file}")"
